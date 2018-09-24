6c432cae05f1ddcf61935dbc0e47e205641da778
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-09-24 15:22:17 +0100

    init repo

---
+++ Trabalho1/MySceneGraph.js
@@ -0,0 +1,75 @@
+var DEGREE_TO_RAD = Math.PI / 180;
+
+// Order of the groups in the XML document.
+var INITIALS_INDEX = 0;
+var ILLUMINATION_INDEX = 1;
+var LIGHTS_INDEX = 2;
+var TEXTURES_INDEX = 3;
+var MATERIALS_INDEX = 4;
+var NODES_INDEX = 5;
+
+/**
+ * MySceneGraph class, representing the scene graph.
+ */
+class MySceneGraph {
+    /**
+     * @constructor
+     */
+    constructor(filename, scene) {
+        this.loadedOk = null;
+
+        // Establish bidirectional references between scene and graph.
+        this.scene = scene;
+        scene.graph = this;
+
+        this.nodes = [];
+
+        this.idRoot = null;                    // The id of the root element.
+
+        this.axisCoords = [];
+        this.axisCoords['x'] = [1, 0, 0];
+        this.axisCoords['y'] = [0, 1, 0];
+        this.axisCoords['z'] = [0, 0, 1];
+
+        // File reading 
+        this.reader = new CGFXMLreader();
+
+        /*
+         * Read the contents of the xml file, and refer to this class for loading and error handlers.
+         * After the file is read, the reader calls onXMLReady on this object.
+         * If any error occurs, the reader calls onXMLError on this object, with an error message
+         */
+
+        this.reader.open('scenes/' + filename, this);
+    }
+
+
+    /*
+     * Callback to be executed after successful reading
+     */
+    onXMLReady() {
+        this.log("XML Loading finished.");
+        var rootElement = this.reader.xmlDoc.documentElement;
+
+        // Here should go the calls for different functions to parse the various blocks
+        var error = this.parseXMLFile(rootElement);
+
+        if (error != null) {
+            this.onXMLError(error);
+            return;
+        }
+
+        this.loadedOk = true;
+
+        // As the graph loaded ok, signal the scene so that any additional initialization depending on the graph can take place
+        this.scene.onGraphLoaded();
+    }
+
+    /**
+     * Parses the XML file, processing each block.
+     * @param {XML root element} rootElement
+     */
+    parseXMLFile(rootElement) {
+        
+    }
+}
