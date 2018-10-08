6c30517ee110160ddbbe05a297415f10374d08f2
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-10-08 16:27:38 +0100

    arranjar normals do cilindro e  do torus e meter bases do cilindro

--- Trabalho1/MyCylinder.js
+++ Trabalho1/MyCylinder.js
@@ -1,94 +1,58 @@
 class MyCylinder extends CGFobject
 {
-	constructor(scene, base, top, height, slices, stacks, minS, maxS, minT, maxT) {
+	constructor(scene, base, top, height, slices, stacks)
+	{
 		super(scene);
-		this.slices = slices;
-        this.stacks = stacks;
+        
         this.base = base;
         this.top = top;
         this.height = height;
-
-		this.minS = minS || 0.0;
-        this.minT = minT || 0.0;
-        this.maxS = maxS || 1.0;
-        this.maxT = maxT || 1.0;
+        this.slices = slices;
+        this.stacks = stacks;
 
 		this.initBuffers();
-	}
+	};
 
-
-	initBuffers() 
+    initBuffers()
 	{
 
-		this.vertices = [];
-		this.normals = [];
-		this.indices = [];
+        this.vertices = [];
+        this.indices = [];
+        this.normals = [];
 		this.texCoords = [];
 
-		//alpha in degrees
-		var alpha = 360/this.slices;
+        var ang=2*Math.PI/this.slices;
+        let dif = this.base - this.top;
+        /*let abs = dif;
+        if(abs < 0)
+            abs = -abs;
+        let incl = Math.tanh(abs/this.height);*/
 
-		//convert to radian
-		alpha = (alpha * Math.PI)/180;
+        for(let j =0; j <= this.stacks; j++){
+            let r = this.base - dif*j/this.stacks;
 
-        //stack increment
-        var stackInc =  this.height/this.stacks;
-        var stackHeight = 0;
-        var prevStackHeight = 0;
-
-        //texture coordinates
-        var texS = 0;
-		var texIncS = 1/this.slices;
-		var texIncT = 1/this.stacks;
-		var prevTexT = 0;
-		var nextTexT = 0;
-
-		var sumalpha = 0;
-
-		for (var j = 0; j < this.stacks; j++) {
-
-		    nextTexT += texIncT;
-		    stackHeight += stackInc;
-
-            for (var i = j*this.slices*4; i < (j+1)*this.slices*4; i += 4) {
-
-                //vertice 1 da face 0
-                this.vertices.push(Math.cos(sumalpha), Math.sin(sumalpha), prevStackHeight);
-                this.normals.push(Math.cos(sumalpha), Math.sin(sumalpha), 0);
-                this.texCoords.push(texS, prevTexT);
-               
-                //vertice 1 da face 1
-                this.vertices.push(Math.cos(sumalpha), Math.sin(sumalpha), stackHeight);
-                this.normals.push(Math.cos(sumalpha), Math.sin(sumalpha), 0);
-                this.texCoords.push(texS, nextTexT);
-        
-                sumalpha += alpha;
-                texS += texIncS;
-
-                //vertice 2 da face 0
-                this.vertices.push(Math.cos(sumalpha), Math.sin(sumalpha), prevStackHeight);
-                this.normals.push(Math.cos(sumalpha), Math.sin(sumalpha), 0);
-                this.texCoords.push(texS, prevTexT);
-
-                //vertice 2 da face 1
-                this.vertices.push(Math.cos(sumalpha), Math.sin(sumalpha), stackHeight);
-                this.normals.push(Math.cos(sumalpha), Math.sin(sumalpha), 0);
-                this.texCoords.push(texS, nextTexT);
-
-                this.indices.push(i + 2);
-                this.indices.push(i + 1);
-                this.indices.push(i);
-                this.indices.push(i + 1);
-                this.indices.push(i + 2);
-                this.indices.push(i + 3);
+            for(let i=0; i < this.slices; i++){
+                this.vertices.push(r*Math.cos(ang *i),r*Math.sin(ang*i),this.height*j/this.stacks);
+                this.normals.push(Math.cos(i*ang),Math.sin(i*ang),0);
+                this.texCoords.push(i/this.slices,j/this.stacks);
             }
-
-            texS = 0;
-            prevTexT = nextTexT;
-            prevStackHeight = stackHeight;
         }
 
-		this.primitiveType = this.scene.gl.TRIANGLES;
-		this.initGLBuffers();
-	};
-};
+        var numPontos=this.stacks*this.slices;
+
+        for (let i =0; i < numPontos; i++){
+            if((i+1)%this.slices==0){
+                this.indices.push(i,i+1-this.slices, i+1);
+                this.indices.push(i,i+1, i+this.slices);
+            }
+            else {
+                this.indices.push(i, i+1, i+1+this.slices);
+                this.indices.push(i, i+1+this.slices, i+this.slices);
+            }
+        }
+        
+
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    };
+}
