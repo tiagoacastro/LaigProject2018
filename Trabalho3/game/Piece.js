35f19b1350b682e9ad9c018b03a035f357266b47
Mariana Costa <mariana@Marianas-MBP.lan>
2019-01-02 18:45:32 +0000

    fixed positions of the pieces

--- Trabalho3/game/Piece.js
+++ Trabalho3/game/Piece.js
@@ -47,27 +47,27 @@ class Piece {
 			switch(this.id) { // 0-2 black, 3-5 white 
 				case 0:
 					this.setPos(2, 1);
-					this.color = 'b';
+					this.color = 'w';
 					break;
 				case 1:
 					this.setPos(3, 4);
-					this.color = 'b';
+					this.color = 'w';
 					break;
 				case 2:
 					this.setPos(4, 1);
-					this.color = 'b';
+					this.color = 'w';
 					break;
 				case 3:
 					this.setPos(2, 5);
-					this.color = 'w';
+					this.color = 'b';
 					break;
 				case 4:
 					this.setPos(3, 2);
-					this.color = 'w';
+					this.color = 'b';
 					break;
 				case 5:
 					this.setPos(4, 5);
-					this.color = 'w';
+					this.color = 'b';
 					break;
 			}
 		}
